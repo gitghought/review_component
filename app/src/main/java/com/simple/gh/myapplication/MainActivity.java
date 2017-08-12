83735ea31db2217244aeb8d8fed86d28fc637873
gitghought <simpleedu_hellp@gmail.com>
2017-08-12 15:16:05 +0000

    add listener to apinner
    add MyLog util to debug

--- app/src/main/java/com/simple/gh/myapplication/MainActivity.java
+++ app/src/main/java/com/simple/gh/myapplication/MainActivity.java
@@ -2,9 +2,13 @@ package com.simple.gh.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
+
+import com.simple.gh.myapplication.utils.MyLog;
 
 import java.util.ArrayList;
 
@@ -23,6 +27,17 @@ public class MainActivity extends AppCompatActivity {
         spinner = (Spinner) findViewById(R.id.spinner_month);
         adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, months);
         spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                MyLog.d(MyLog.TAG, "onItemClick -> " + months.get(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
     private void initMonth() {
