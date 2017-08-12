cde7d2224e4aae08c91e2f98d3c348a749949001
gitghought <simpleedu_hellp@gmail.com>
2017-08-12 15:45:10 +0000

    add sub spinner

--- app/src/main/java/com/simple/gh/myapplication/MainActivity.java
+++ app/src/main/java/com/simple/gh/myapplication/MainActivity.java
@@ -15,22 +15,38 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     private Spinner spinner;
     private SpinnerAdapter adapter;
-    private ArrayList<String> months = new ArrayList<>();
+    private ArrayList<String> seasons = new ArrayList<>();
+
+    private Spinner spineer_month;
+    private SpinnerAdapter adapter_month;
+    private String months[][] = new String [][] {
+        {"month1", "month2", "month3"},
+        {"month4", "month5", "month6"},
+        {"month7", "month8", "month9"},
+        {"month10", "month11", "month12"}
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initMonth();
+        initSeason();
 
-        spinner = (Spinner) findViewById(R.id.spinner_month);
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, months);
+        spinner = (Spinner) findViewById(R.id.spinner_season);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, seasons);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                MyLog.d(MyLog.TAG, "onItemClick -> " + months.get(position));
+                MyLog.d(MyLog.TAG, "onItemClick -> " + seasons.get(position));
+                spineer_month = (Spinner) findViewById(R.id.spinner_month);
+                adapter_month = new ArrayAdapter<String>(
+                        MainActivity.this,
+                        android.R.layout.simple_list_item_1,
+                        months[position]
+                );
+                spineer_month.setAdapter(adapter_month);
             }
 
             @Override
@@ -40,9 +56,9 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void initMonth() {
-        for (int i = 0; i < 3; i++) {
-            months.add("month " + i);
+    private void initSeason() {
+        for (int i = 0; i < 4; i++) {
+            seasons.add("Seasons " + i);
         }
     }
 }
