e3964b97c3e9086a73e950d5c19224d56da4caf5
gitghought <simpleedu_hellp@gmail.com>
2017-08-13 22:44:36 +0000

    添加两个Fragment

---
+++ app/src/main/java/com/simple/gh/myapplication/fragment/FragmentFind.java
@@ -0,0 +1,15 @@
+package com.simple.gh.myapplication.fragment;
+
+import com.simple.gh.myapplication.R;
+
+/**
+ * Created by gh on 2017-08-13.
+ */
+
+public class FragmentFind extends SingleFragment {
+
+    @Override
+    public int getResource() {
+        return R.layout.tab3;
+    }
+}
---
+++ app/src/main/java/com/simple/gh/myapplication/fragment/FragmentMe.java
@@ -0,0 +1,15 @@
+package com.simple.gh.myapplication.fragment;
+
+import com.simple.gh.myapplication.R;
+
+/**
+ * Created by gh on 2017-08-13.
+ */
+
+public class FragmentMe extends SingleFragment {
+
+    @Override
+    public int getResource() {
+        return R.layout.tab4;
+    }
+}
