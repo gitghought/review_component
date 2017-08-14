a5ca92b715741cf803135c75801378bcf16acd74
gitghought <simpleedu_hellp@gmail.com>
2017-08-14 00:05:23 +0000

    基本实现menu功能

--- app/src/main/java/com/simple/gh/myapplication/MainActivity.java
+++ app/src/main/java/com/simple/gh/myapplication/MainActivity.java
@@ -2,6 +2,12 @@ package com.simple.gh.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.simple.gh.myapplication.utils.MyLog;
+
+import static android.R.attr.id;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.menu_one:
+                MyLog.d(MyLog.TAG, "menu one");
+                break;
+            case R.id.menu_two:
+                MyLog.d(MyLog.TAG, "menu two");
+                break;
+            case R.id.menu_three:
+                MyLog.d(MyLog.TAG, "menu three");
+                break;
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean onMenuOpened(int featureId, Menu menu) {
+        MyLog.d(MyLog.TAG, "the menu opened");
+
+//    return super.onMenuOpened(featureId, menu);
+        return true;
+    }
+
+//    @Override
+//    public void onContextMenuClosed(Menu menu) {
+//
+//        super.onContextMenuClosed(menu);
+//        MyLog.d(MyLog.TAG, "the menu closed");
+//    }
+
+    @Override
+    public void onOptionsMenuClosed(Menu menu) {
+//        super.onOptionsMenuClosed(menu);
+        MyLog.d(MyLog.TAG, "the menu closed");
     }
 }
---
+++ app/src/main/java/com/simple/gh/myapplication/utils/MyLog.java
@@ -0,0 +1,18 @@
+package com.simple.gh.myapplication.utils;
+
+import android.util.Log;
+
+/**
+ * Created by gh on 2017-08-09.
+ */
+
+public class MyLog {
+    public static final String TAG = "MyLog";
+    public static StackTraceElement[] eles /*= new Throwable().getStackTrace()*/;
+
+    public static void d(String TAG, String msg) {
+        eles = new Throwable().getStackTrace();
+        Log.d(TAG, "in " + eles[1].getFileName() + ":" + eles[1].getMethodName() + "@" +  eles[1].getLineNumber() + msg);
+    }
+
+}
