f1eb52287eed1d945344edc2f0cf687255e79941
gitghought <simpleedu_hellp@gmail.com>
2017-08-29 14:13:36 +0000

    添加Button

--- app/src/main/java/com/simple/gh/myapplication/MainActivity.java
+++ app/src/main/java/com/simple/gh/myapplication/MainActivity.java
@@ -2,12 +2,31 @@ package com.simple.gh.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    //创建TextView对象
+    private TextView tvShow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //初始化TextView对象
+        tvShow = (TextView) findViewById(R.id.tv_show);
+
+        //添加TextView空间逻辑功能
+        tvShow.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this, "Hello world", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
     }
 }
