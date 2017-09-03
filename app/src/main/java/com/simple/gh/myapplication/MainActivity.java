2d47fbad46144b5dcd61691f742003a6b9019871
gitghought <simpleedu_hellp@gmail.com>
2017-09-03 14:51:18 +0000

    实现ImageButton控件的基本使用、进阶使用

--- app/src/main/java/com/simple/gh/myapplication/MainActivity.java
+++ app/src/main/java/com/simple/gh/myapplication/MainActivity.java
@@ -1,66 +1,39 @@
 package com.simple.gh.myapplication;
 
+import android.graphics.drawable.BitmapDrawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.ImageButton;
 
 import com.simple.gh.myapplication.utils.MyLog;
 
-import static android.R.attr.id;
-
 public class MainActivity extends AppCompatActivity {
+    private ImageButton ibShow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initButton();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
-        menu.add(Menu.NONE, 1,6,"menu 5");
-
-        return true;
+    public void touch(View vv) {
+        MyLog.d(MyLog.TAG, "touch");
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        switch (id) {
-            case R.id.menu_one:
-                MyLog.d(MyLog.TAG, "menu one");
-                break;
-            case R.id.menu_two:
-                MyLog.d(MyLog.TAG, "menu two");
-                break;
-            case R.id.menu_three:
-                MyLog.d(MyLog.TAG, "menu three");
-                break;
-        }
-
-        return true;
-    }
-
-    @Override
-    public boolean onMenuOpened(int featureId, Menu menu) {
-        MyLog.d(MyLog.TAG, "the menu opened");
-
-//    return super.onMenuOpened(featureId, menu);
-        return true;
-    }
-
-//    @Override
-//    public void onContextMenuClosed(Menu menu) {
-//
-//        super.onContextMenuClosed(menu);
-//        MyLog.d(MyLog.TAG, "the menu closed");
-//    }
-
-    @Override
-    public void onOptionsMenuClosed(Menu menu) {
-//        super.onOptionsMenuClosed(menu);
-        MyLog.d(MyLog.TAG, "the menu closed");
+    private void initButton() {
+        this.ibShow = (ImageButton) findViewById(R.id.ib_show);
+        BitmapDrawable bd = (BitmapDrawable) ContextCompat.getDrawable(this, R.mipmap.ic_launcher);
+        this.ibShow.setImageDrawable(bd);
+        this.ibShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyLog.d(MyLog.TAG, "touch sub");
+            }
+        });
     }
 }
