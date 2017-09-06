368795cd5f40ddf70ce5d7fd7ea158dc86469312
gitghought <simpleedu_hellp@gmail.com>
2017-09-06 14:57:44 +0000

    完成创建数据库表

--- app/src/main/java/com/simple/gh/myapplication/MainActivity.java
+++ app/src/main/java/com/simple/gh/myapplication/MainActivity.java
@@ -1,66 +1,45 @@
 package com.simple.gh.myapplication;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
+import com.simple.gh.myapplication.sqlite.MySQLite;
 import com.simple.gh.myapplication.utils.MyLog;
 
 import static android.R.attr.id;
 
 public class MainActivity extends AppCompatActivity {
+    private MySQLite sql;
+    private Button btnGetSql;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initButton();
+        initSQLite();
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
-        menu.add(Menu.NONE, 1,6,"menu 5");
+    private void initButton() {
+        this.btnGetSql = (Button) findViewById(R.id.btn_get_sql);
+        this.btnGetSql.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = sql.getWritableDatabase();
+            }
+        });
 
-        return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        switch (id) {
-            case R.id.menu_one:
-                MyLog.d(MyLog.TAG, "menu one");
-                break;
-            case R.id.menu_two:
-                MyLog.d(MyLog.TAG, "menu two");
-                break;
-            case R.id.menu_three:
-                MyLog.d(MyLog.TAG, "menu three");
-                break;
-        }
-
-        return true;
+    private void initSQLite() {
+        this.sql = new MySQLite(this, "MyContact", null, 1);
     }
 
-    @Override
-    public boolean onMenuOpened(int featureId, Menu menu) {
-        MyLog.d(MyLog.TAG, "the menu opened");
-
-//    return super.onMenuOpened(featureId, menu);
-        return true;
-    }
-
-//    @Override
-//    public void onContextMenuClosed(Menu menu) {
-//
-//        super.onContextMenuClosed(menu);
-//        MyLog.d(MyLog.TAG, "the menu closed");
-//    }
-
-    @Override
-    public void onOptionsMenuClosed(Menu menu) {
-//        super.onOptionsMenuClosed(menu);
-        MyLog.d(MyLog.TAG, "the menu closed");
-    }
 }
