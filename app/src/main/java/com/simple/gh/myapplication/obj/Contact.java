789de2b3c8571566f25ab0226c9bcf86f148969a
gitghought <simpleedu_hellp@gmail.com>
2017-09-09 02:19:00 +0000

    clean for master-detail

---
+++ app/src/main/java/com/simple/gh/myapplication/obj/Contact.java
@@ -0,0 +1,26 @@
+package com.simple.gh.myapplication.obj;
+
+/**
+ * Created by gh on 2017-09-09.
+ */
+
+public class Contact {
+    private String name;
+    private String phone;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
